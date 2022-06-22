7fefea35213785c276cccc99b74f3be6fc5194be
Chae-G-0 <103270067+Chae-G-0@users.noreply.github.com>
2022-06-22 12:06:41 +0900

    Add files via upload

--- trn02/js/main.js
+++ trn02/js/main.js
@@ -9,7 +9,7 @@ $(function () {
         var current = $('.slick-current');
         current.addClass('on').siblings().removeClass('on');
         $('.mainArrow i').removeClass('on');
-        if(c === 1) {
+        if (c === 1) {
             $('.mainArrow i').addClass('on')
         }
         $('.slideNum').text((c ? (c + 1) : 1) + " / " + s.slideCount);
@@ -23,11 +23,30 @@ $(function () {
         pauseOnFocus: false,
     });
 
-    $('.mainArrow i:nth-child(1)').on('click', function(){
+    $('.mainArrow i:nth-child(1)').on('click', function () {
         $('.mainSlider').slick('slickPrev')
     });
 
-    $('.mainArrow i:nth-child(2)').on('click', function(){
+    $('.mainArrow i:nth-child(2)').on('click', function () {
         $('.mainSlider').slick('slickNext')
     });
+
+    $('#utb').YTPlayer({
+        videoURL: 'https://youtu.be/dS9x5Ws67wI',
+        containment: '.utb',
+        autoPlay: true,
+        mute: true,
+        startAt: 0,
+        opacity: 1,
+        showControls: false,
+        playOnlyIfVisible: true,
+    });
+
+    $('.utb .zoom').on('click', function (e) {
+        e.preventDefault();
+        $('#utb').YTPFullscreen()
+    })
+
+
+
 })
