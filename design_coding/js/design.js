abeb64230c7639a92f46e3541b4697c35182c942
Chae-G-0 <103270067+Chae-G-0@users.noreply.github.com>
2022-06-23 17:51:03 +0900

    Add files via upload

--- design_coding/js/design.js
+++ design_coding/js/design.js
@@ -4,5 +4,14 @@ $(function () {
         dots: true,
         slidesToShow: 1,
         centerMode: true,
+        centerPadding: '150px'
+    });
+
+    $('.mainslider .arrows i:nth-child(1)').on('click', function () {
+        $('.mainslider .slider').slick('slickPrev')
+    });
+
+    $('.mainslider .arrows i:nth-child(2)').on('click', function () {
+        $('.mainslider .slider').slick('slickNext')
     });
 })
